6646a2e81eb7898d65800759e0cd7c04e3c708b1
anyega <anyega.alex.kamau@gmail.com>
2023-03-23 02:33:00 +0300

    Cards Section Working

--- main.js
+++ main.js
@@ -5,12 +5,14 @@ let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
 let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 let sum = firstCard + secondCard
 
 
   function startGame(){
     sumEl.innerText = "Sum :" + sum
+    cardsEl.textContent = "Cards :" + firstCard +" "+ secondCard
     if (sum <= 20) {
       message = "Do you want to draw a new card? "
       } else if (sum === 21) {
