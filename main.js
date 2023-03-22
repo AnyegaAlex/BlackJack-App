3ce5c7e07726387c87f45f40420f3fdab4e61a97
anyega <anyega.alex.kamau@gmail.com>
2023-03-23 02:15:47 +0300

    Changes the message on message

--- main.js
+++ main.js
@@ -3,17 +3,31 @@ let secondCard = 10
 let hasBlackJack = false
 let isAlive = true
 let message = ""
+let messageEl = document.getElementById("message-el")
 
 let sum = firstCard + secondCard
 
-if (sum <= 20) {
-  message = "Do you want to draw a new card? 🙂 "
-  } else if (sum === 21) {
-  message ="Woohoo! You've got Blackjack! 🥳 "
-  hasBlackJack = true
-  } else {
-  message = "You're out of the game! 😭 "
-  isAlive = false
+
+  function startGame(){
+    if (sum <= 20) {
+      message = "Do you want to draw a new card? 🙂 "
+      } else if (sum === 21) {
+      message ="Woohoo! You've got Blackjack! 🥳 "
+      hasBlackJack = true
+      } else {
+      message = "You're out of the game! 😭 "
+      isAlive = false
+      }
+      messageEl.innerHTML = message
   }
 
-  console.log(message)
+  
+
+
+
+  
+  
+  
+  
+  
+
---
+++ test.js
@@ -0,0 +1,17 @@
+// let age = 22
+// 
+// if (age < 21){
+  // console.log("You can not enter the club!")
+// } else {
+  // console.log(" Welcome!")
+// }
+
+let age = 200
+
+if (age < 100){
+  console.log("Not elegible")
+}else if (age === 100){
+  console.log("Here is your birthday card from th King!")
+}else{
+  console.log("Not elegible, you have already gotten one")
+}
